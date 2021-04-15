d1917f3357edafddff58001d22219c258f67b2e9
Kaysie Yu <kaysie.yu@gmail.com>
2021-04-14 21:45:41 -0700

    Implemented basic jaccard similarity scoring

---
+++ app/irsystem/controllers/jaccard.py
@@ -0,0 +1,93 @@
+import pandas as pd
+import numpy as np
+from nltk.tokenize import TreebankWordTokenizer
+
+# load in the data
+df = pd.read_csv('data/hotaling_cocktails - Cocktails.csv')
+
+# initalized nltk tokenizer function
+treebank_tokenizer = TreebankWordTokenizer()
+
+# drop Bartender, Bar/Company, Location columns from df
+input_df = df.drop(['Bartender', 'Bar/Company', 'Location'], axis=1)
+
+# combine all columns in input_df for each row    
+sm_df = input_df['Cocktail Name'].map(str) + ' ' + input_df['Ingredients'].map(str) + ' ' + input_df['Garnish'].map(str) + ' ' + input_df['Glassware'].map(str) + ' ' + input_df['Preparation'].map(str) + ' ' + input_df['Notes'].map(str)
+
+# tokenize, lowercase, and remove punctuation from sm_df
+cocktail_names = df.iloc[:,0]
+for idx,name in enumerate(cocktail_names):
+    tokenized_name = name.split()
+    cocktail_names[idx] = [w.lower() for w in tokenized_name if w.isalpha()]
+
+# compute jaccard comparing the query terms against sm_df
+def jaccard(input_query, input_data, num_cocktails, tokenizer=treebank_tokenizer):
+    """Return a list of length num_cocktails where index i is the Jaccard 
+    similarity between the query terms and the cocktail name
+    
+    Params: {query: String,
+             data: Pandas df,
+             num_cocktails: Integer,
+             tokenizer: a TreebankWordTokenizer}
+    Returns: np.ndarray
+    """
+    
+    # initialize array that will contain similarity scores
+    jac_sim = np.zeros((1, num_cocktails))
+    
+    # tokenize, lowercase, and remove punctuation from input_query 
+    input_query = str(input_query)
+    tok_list = tokenizer.tokenize(input_query)
+    
+    query = [q.lower() for q in tok_list if q.isalpha()]
+    
+    # loop through cocktail name col and compute the jaccard similarity score
+    for idx,name in enumerate(cocktail_names):
+        
+        # calculate num and denom for the ratio
+        num = len(list(set(name).intersection(query)))
+        denom = len(list(set(name).union(query)))
+        
+        if denom != 0:   # include in case the union of two sets is nothing
+            ratio = num/denom
+
+            # update the jac_sim ndarray with the jaccard similarity score
+            jac_sim[0,idx] = ratio
+        
+    return jac_sim
+
+# initalize variables for jaccard function call 
+num_cocktails = df.shape[0]
+
+# retrieve the top 10 most similar names to the query term
+def top_scores(jac_sim):
+    """Return a list of tuples (index, score) containing the top ten jaccard similarity scores
+    
+    Params: {jac_sim: np.ndarray}
+    Returns: List
+    """
+
+    # map the scores to the index in a list of tuples
+    jaccard_scores = []
+    for idx,score in enumerate(jac_sim[0]):
+        jaccard_scores.append((idx,score))
+        
+    # sort scores in ascending order
+    jaccard_sorted = sorted(jaccard_scores, key=lambda x: -x[1])
+
+    # get the top 10 indices and scores
+    top_10 = []
+    if len(jaccard_sorted) > 10:
+        top_10.append(jaccard_sorted[:10])
+    else:
+        top_10.append(jaccard_sorted)
+
+    # get the top 10 info to print on UI
+    top_10_info = []
+    for i in top_10[0]:
+        idx = i[0]  
+        top_10_info.append(input_df.iloc[idx,0])  
+    # print(input_df.iloc[idx,:], "\n")   # grabs all info
+
+    return top_10_info
+
--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -1,20 +1,26 @@
 from . import *  
 from app.irsystem.models.helpers import *
 from app.irsystem.models.helpers import NumpyEncoder as NumpyEncoder
+from app.irsystem.controllers.jaccard import *
 
 project_name = "Liver Let Die - Personalized Cocktail Recommendations"
 net_id = "Annika Opsasnick (aro42), Callie Aboaf (cha46), Kaysie Yu (ky276), Simran Puri (sp2262), Yunyun Wang (yw458)"
 
 @irsystem.route('/', methods=['GET'])
+
+# function that generates everything on search.html
 def search():
 	query = request.args.get('search')
+
+	jaccard_sim = jaccard(query, sm_df, num_cocktails, treebank_tokenizer)
+	scores = top_scores(jaccard_sim)
+	
 	if not query:
 		data = []
 		output_message = ''
 	else:
 		output_message = "Your search: " + query
-		data = range(5)
+		data = scores
 	return render_template('search.html', name=project_name, netid=net_id, output_message=output_message, data=data)
-
 
 
