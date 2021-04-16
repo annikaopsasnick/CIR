25ccc689a2d6f5b883fca82599dca24d99b00bfa
Annika <aro42@cornell.edu>
2021-04-15 20:22:37 -0400

    basic message

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,7 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
 
 function App() {
+  const [getMessage, setGetMessage] = useState({})
+
+  useEffect( ()=>{
+    axios.get('/')
+    .then(response => {
+      console.log("SUCCESS", response)
+      setGetMessage(response)
+    })
+    .catch(error => {
+      console.log(error)
+    })
+  },[])
+  
   return (
     <div className="App">
       <header className="App-header">
