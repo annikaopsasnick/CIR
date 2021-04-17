1d5e2592ecc7f8f5bbb472f476a42e530cc0d462
Yunyun Wang <ys.wang625@gmail.com>
2021-04-17 14:37:51 -0400

    updated form input fields

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -2,13 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { Form } from './Form.js';
+import Form from './Form.js';
 
 
 function App() {
   const [getMessage, setGetMessage] = useState({})
   const [name, setName] = useState("myname")
   const [netid, setNetID] = useState("myid")
+  const [inputs, setInputs] = useState({'user_name': '', 'key_words': '', 'base_spirit': 'vodka', 'ingredients': ['gin', 'lemon']})
 
   useEffect(() => {
     axios.get('/')
@@ -29,6 +30,10 @@ function App() {
       <body>
       <div className="form-container">Test
         < Form
+        inputs= {inputs} 
+        setInputs = {setInputs}
+     
+
         onSubmit={(x) => setName(x)}/>
         
         </div>
@@ -36,6 +41,7 @@ function App() {
         <div class="topcorner">
           <p>Project Name: {name}</p>
           <p>Student Names: {netid}</p>
+
         </div>
         
 
--- webapp/src/Form.js
+++ webapp/src/Form.js
@@ -1,34 +1,84 @@
-import React from 'react';
+//import React from 'react';
+const Form = ({inputs, setInputs}) => {
+  
+//class Form extends React.Component {
+  // constructor(props) {
+  //   super(props);
+  //   this.state = { value: '' };
 
-class Form extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { value: '' };
+  //   this.handleChange = this.handleChange.bind(this);
+  //   this.handleSubmit = this.handleSubmit.bind(this);
+  // }
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+  function handleChange(e) { //output 
+   // this.setState({ value: key.target.value });
+   let key = e.target.name;
+   let value = e.target.value;
+    inputs[key]= value;
+    setInputs({...inputs});
+    
+    console.log(inputs);
   }
 
-  handleChange(event) {
-    this.setState({ value: event.target.value });
-  }
-
-  handleSubmit(event) {
+  function handleSubmit(event) {
     this.props.onSubmit(this.state.value);
     event.preventDefault();
-  }
 
-  render() {
+  }
     return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Name:
-          <input type="text" value={this.state.value} onChange={this.handleChange} />
+      <form onSubmit={handleSubmit}>
+        <div class = "input-pair">
+          <label>
+            Name:
+            <input type="text" name= "user_name" onChange={handleChange} />
+          </label>
+
+          <input type="submit" value="Submit" />
+        </div>
+
+        <div class = "input-pair">
+
+          <label>
+            Key Word: 
+            
+          </label>
+          <input type = "text" id="key-words" name= "key_word" onChange={handleChange} />
+        </div>
+
+        <div class = "input-pair">
+          <label>
+            Base Spirit: 
+          </label>
+
+          <select id="base_spirit">
+            <option value= {inputs.base_spirit} selected>--Please choose a base spirit--</option>
+            <option value="vodka">Vodka</option>
+            <option value ="tequila">Tequila</option>
+            <option value ="gin">Gin</option>
+            <option value ="rum">Rum</option>
+            <option value ="whiskey">Whiskey</option>
+            <option value ="brandy">Brandy</option>
+          </select>
+        </div>
+
+        <label class="switch">
+          Mocktail?
+          <input id="mock" type="checkbox"/>
+          <span class="slider round"></span>
         </label>
-        <input type="submit" value="Submit" />
+        
+
+        <label class="switch">
+          Iced?
+          <input id="ice" type="checkbox"/>
+          <span class="slider round"></span>
+        </label>
+        
+
+
       </form>
     );
   }
-}
 
-export { Form }
+
+export default Form
