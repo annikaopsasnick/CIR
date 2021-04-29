83ae0f1f6286175b1cb80c746b70d57e9c481f79
Annika <aro42@cornell.edu>
2021-04-29 10:36:01 -0400

    top scores atomic and ability to get invalid results

--- app/irsystem/controllers/jaccard.py
+++ app/irsystem/controllers/jaccard.py
@@ -79,32 +79,47 @@ num_cocktails = df.shape[0]
 def top_scores(jac_sim):
     """Return a list of row object where key is the column name, containing the top ten jaccard similarity scores
     
-    Params: {jac_sim: np.ndarray}
-    Returns: List
+    Params: {jac_sim: Pandas Series}
+    Returns: List of json objects
     """
+      
+    jaccard_sorted = jac_sim.sort_values(ascending=False) # sort in descending order 
+    top_10 = jaccard_sorted.head(10)  # get the top 10 indices and scores
+    top_10 = top_10[top_10!=0]    # remove zero scores
 
-    # map the scores to the index in a list of tuples
-    jaccard_scores = []
-    for idx,score in jac_sim.iteritems():
-        print(idx,score)
-        jaccard_scores.append((idx,score))
-        
-    # sort scores in ascending order
-    jaccard_sorted = sorted(jaccard_scores, key=lambda x: -x[1])
+    print(top_10)
 
-    # get the top 10 indices and scores
-    top_10 = []
-    if len(jaccard_sorted) > 10:
-        top_10.append(jaccard_sorted[:10])
-    else:
-        top_10.append(jaccard_sorted)
-
-    # get the top 10 info to print on UI (name, ingredients, description, url, image)
-    top_10_idx = [idx[0] for idx in top_10[0]]
-    top_10_df = df.iloc[top_10_idx, :]
+    top_10_idx = top_10.index.tolist() # get list of indexes
+    top_10_df = df.iloc[top_10_idx, :] # get rows of top "ten" indexes
     top_10_json = top_10_df.to_json(orient='records')
 
     return top_10_json
+
+
+    """
+        # map the scores to the index in a list of tuples
+        jaccard_scores = []
+        for idx,score in jac_sim.iteritems():
+            print(idx,score)
+            jaccard_scores.append((idx,score))
+            
+        # sort scores in ascending order
+        jaccard_sorted = sorted(jaccard_scores, key=lambda x: -x[1])
+
+        # get the top 10 indices and scores
+        top_10 = []
+        if len(jaccard_sorted) > 10:
+            top_10.append(jaccard_sorted[:10])
+        else:
+            top_10.append(jaccard_sorted)
+
+        # get the top 10 info to print on UI (name, ingredients, description, url, image)
+        top_10_idx = [idx[0] for idx in top_10[0]]
+        top_10_df = df.iloc[top_10_idx, :]
+        top_10_json = top_10_df.to_json(orient='records')
+
+        return top_10_json
+    """
 
 
 
