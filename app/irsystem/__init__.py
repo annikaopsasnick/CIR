2af30fdb98e6900dd0da560fa443dd5f2cdea85f
Kaysie Yu <kaysie.yu@gmail.com>
2021-04-30 13:45:49 -0400

    init.py debug

--- app/irsystem/__init__.py
+++ app/irsystem/__init__.py
@@ -1,7 +1,16 @@
 from flask import Blueprint
+import os
+from pathlib import Path
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+basedir = Path(basedir)
+templatefolder = str(basedir.parent.parent /"webapp"/"build")
+staticfolder = str(basedir.parent.parent /"webapp"/"build"/"static")
+
 
 # Define a Blueprint for this module (mchat)
-irsystem = Blueprint('irsystem', __name__, url_prefix='/',template_folder="../webapp/build", static_folder="../webapp/build/static")
+irsystem = Blueprint('irsystem', __name__, url_prefix='/',template_folder=templatefolder, static_folder=staticfolder)
 
 # Import all controllers
 from .controllers.search_controller import *
+
