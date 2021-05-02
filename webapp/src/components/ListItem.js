9fffa60901d8aa735c78a4a7a9e04a46f2097fc5
Yunyun Wang <ys.wang625@gmail.com>
2021-05-02 01:40:10 -0400

    added styling listview

--- webapp/src/components/ListItem.js
+++ webapp/src/components/ListItem.js
@@ -7,20 +7,35 @@ const ListItem = (props) => {
       <button onClick={() => {
         props.ViewCocktail(props.name) // open specific cocktail details
       }}>
-        <h2>{props.name}</h2>
-        <div class="row">
-          <img src={props.image_source} alt="cocktail" />
-          <p>{props.ingredients}</p>
-        </div>
-        <div class = "rating">
-          <h3>Social rating:  </h3>
-          <h3>{props.rating}</h3>
+        <div className = "ListItem">
+          <div className="Cocktail_title">
+          <h2>{props.name}</h2>
+          </div>
+        
+            <div className="row">
+              <div className="text-box" id="cocktail-image">
+                <img src={props.image_source} alt="cocktail" />
+              </div>
+              
+              <div className="text-box" id="ingredients">
+                <p>{props.ingredients}</p>
+              </div>
+
+            </div>
+    
+          <div className = "rating">
+            <h3>Social rating:  </h3>
+            <h3>{props.rating}</h3>
+          </div>
+
         </div>
       </button>
     </li>
+
   )
 
 }
+
 
 ListItem.propTypes = {
   ingredients: PropTypes.array.isRequired,
